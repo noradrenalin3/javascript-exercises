e3ab74e626e6669e90e439f2402c51da53925231
Lion Puro <167165944+noradrenalin3@users.noreply.github.com>
2024-05-28 14:59:08 +0300

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.reduce((acc, curr) => acc + curr, 0) === undefined) {
+		return 0;
+	} else {
+		return arr.reduce((acc, curr) => acc + curr, 0);
+	}
+}
+
+const multiply = function(arr) {
+	return arr.reduce((acc, curr) => acc * curr);
+}
+
+const power = function(n, p) {
+	return n ** p;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num){
+    if (num === 0 || num === 1) {
+        return 1;
+    } else {
+        //const numbers = Array.from({ length: num }, (v, i) => i+1); 
+        return Array.from({length: num})
+            .map((v, i) => i + 1)
+            .reduce((a, b) => a * b, 1); 
+    };  
+}
 
 // Do not edit below this line
 module.exports = {
