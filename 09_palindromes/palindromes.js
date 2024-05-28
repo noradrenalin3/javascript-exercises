2841d2dc6406f4cbb2be2bd016f60ac9176031ea
Lion Puro <167165944+noradrenalin3@users.noreply.github.com>
2024-05-28 18:44:17 +0300

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
-};
+const palindromes = function(w) {
+	//const myArray = Array.from(w);
+	//const filtered = myArray
+	const filtered = w
+		.toLowerCase()
+		.split('')
+		.filter(char => /[a-zA-Z0-9]/.test(char))
+		//.toString();
+		.join('');
+	//const stringToArr = Array.from(filtered);
+	const reversed = filtered
+		.split('')
+		.reverse()
+		.join('');
+    return filtered === reversed;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
