2a43be23cdb99dfc5b1bb4bd0c6417ab78d1422e
Lion Puro <167165944+noradrenalin3@users.noreply.github.com>
2024-05-29 13:17:37 +0300

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const currentYear = (new Date()).getFullYear();
+    const age = array.map((person) => {
+        return {...person, age: calcAge(person.yearOfDeath, person.yearOfBirth)};
+    }); 
+    function calcAge(d, b) {
+        if (!d) {
+            return currentYear - b;
+        } else {
+            return d - b;
+        }   
+    };   
+    const ordered = age.sort(function(a, b) {
+        const ageA = a.age;
+        const ageB = b.age;
+        if (ageA > ageB) {
+            return 1;
+        } else {
+            return -1;    
+        }
+    }); 
+    const oldest = ordered.pop();
+    return oldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
