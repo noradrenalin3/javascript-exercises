0450067f6057188c5418867c187bb765726d2d82
Lion Puro <167165944+noradrenalin3@users.noreply.github.com>
2024-05-29 10:26:56 +0300

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+		return 'OOPS';
+	}
+    if (num < 2) {
+		return Number(num);
+	}
+    else {
+        function formula(n = 0) {
+            if (n < 0) {return 'OOPS'}
+            if (n < 2) {return n};
+            return formula(n - 1) + formula(n -2);
+        }
+        function fibonacciGen() {
+            let index = 1;
+            return function() {
+                return formula(index++);
+            }
+        }
 
+        let fibonacciSequence = fibonacciGen();
+
+        const numbers = [];
+        for (let i = 0; i < num; i++) {
+            numbers.push(fibonacciSequence());
+        }
+		return Number(numbers.pop());
+	}
 };
 
 // Do not edit below this line
